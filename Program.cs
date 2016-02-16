1345ecdec15fbed0dbb613d3be06dd5bc16fffcf
kociumbas <kociumbas@outlook.com>
2016-02-16 01:05:55 +0100

    CSPoleProstokataConsole

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1poleProstokataConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, pole = 0;
+            Console.WriteLine("Program oblicza pole prostokata o bokach a i b");
+            Console.Write("Podaj długość boku a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj długość boku b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+            pole = a * b;
+            Console.Write("Pole prostokata o bokach a = {0} i b = {1}", a, b);
+            Console.WriteLine(" wynosi {0}", pole);
+            Console.ReadLine();
+        }
+    }
+}
